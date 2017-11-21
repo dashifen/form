aff5889cde22e427049cef30e32162584518ed53
David Dashifen Kees <dashifen@dashifen.com>
2017-11-21 17:04:57 -0500

    added File entry

---
+++ src/Fields/Elements/Entries/File.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dashifen\Form\Fields\Elements\Entries;
+
+class File extends Text {
+
+	// this one, like the password object, doesn't need any more work.
+	// by changing the name of the class, it'll automatically return the
+	// word "file" for the type and everything else can stay the same.
+
+}
